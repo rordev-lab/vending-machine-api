e788bd770ea1a6f916a97153d0df277cd3e443f6
KapilCh <kapilch112@gmail.com>
2021-09-06 13:47:50 +0530

    Added deposit endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,7 @@ module Api
     class UsersController < ApplicationController
       before_action :authenticate_api_user!
       before_action :set_user, only: %i[show update destroy]
+      before_action :validate_buyer, only: :deposit
 
       # GET /api/v1/users
       def index
@@ -33,6 +34,15 @@ module Api
         render json: { message: 'User deleted successfully' }, status: :ok
       end
 
+      def deposit
+        @user = User.find(params[:user_id])
+        if @user.update(deposit_params)
+          render json: @user
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
       private
 
       # Use callbacks to share common setup or constraints between actions.
@@ -42,7 +52,20 @@ module Api
 
       # Only allow a list of trusted parameters through.
       def user_params
-        params.require(:user).permit(:username, :email, :role, :deposit)
+        params.require(:user).permit(:username, :email, :role)
+      end
+
+      def deposit_params
+        params.require(:user).permit(:deposit)
+      end
+
+      def validate_buyer
+        return if current_api_user.buyer?
+
+        render json: {
+          error: 'Only buyer can deposit money',
+          status: :unprocessable_entity
+        }
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :products
-      resources :users, except: [:create, :edit]
+      resources :users, except: [:create, :edit] do
+        put :deposit
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
